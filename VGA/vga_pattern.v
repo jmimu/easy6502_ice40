2378e8dfd8de93cc7af4ae1327ea874bffce0784
jmimu <jmimu@free.fr>
2023-04-07 18:47:15 +0200

    vga 13h default palette

--- VGA/vga_pattern.v
+++ VGA/vga_pattern.v
@@ -27,8 +27,6 @@ module vga_pattern(clk, reset, hsync, vsync, rgb);
     .vmaxxed(vmaxxed)
   );
 
-  wire [11:0] rgb_out;
-
   wire r = display_on && (((hpos&7)==0) || ((vpos&7)==0));
   wire g = display_on && vpos[4];
   wire b = display_on;// && hpos[4];
@@ -75,22 +73,23 @@ module vga_pattern(clk, reset, hsync, vsync, rgb);
         suby <= suby + 5'b1;
     end
   end
-/*  
-  wire [7:0] palettes_addr = {outpix?5'b0:pixx , r, g, b};
-  generic_ram #(.DATA_WIDTH(12),.ADDR_WIDTH(8),.IN_FILENAME("../palettes.mem"))
+ 
+  wire [7:0] palettes_addr = {2'b0, pixy[2:0], pixx};
+  wire [23:0] rgb_out; // TODO: 15 bit palette
+  generic_ram #(.DATA_WIDTH(24),.ADDR_WIDTH(8),.IN_FILENAME("../palettes.mem"))
   ram_palettes_data(
       .rclk(clk),
       .wclk(clk),
       .write_en(1'b0),
       .waddr(8'b0),
-      .din(12'b0),
+      .din(24'b0),
       .raddr(palettes_addr),
       .dout(rgb_out));
 
-  assign rgb = { {rgb_out[11:8],1'b0}, {rgb_out[7:4],1'b0}, {rgb_out[3:0],1'b0}};
-*/
-  wire borderpixx = (pixx==0)||(pixx==31);
+  assign rgb =  outpix ? 15'b0 : { rgb_out[23:19], rgb_out[15:11], rgb_out[7:3] }; // TODO: delay outpix
+
+  /*wire borderpixx = (pixx==0)||(pixx==31);
   wire borderpixy = (pixy==0)||(pixy==31);
   
-  assign rgb = { outpix?5'b0:{borderpixx,4'b0}, outpix?5'b0:{borderpixy,4'b0}, 5'b0};
+  assign rgb = { outpix?5'b0:{borderpixx,4'b0}, outpix?5'b0:{borderpixy,4'b0}, 5'b0};*/
 endmodule
